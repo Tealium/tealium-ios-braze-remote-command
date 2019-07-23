f0b37e71fe7ede8eff25920cb42c46ea2cd0344f
jonathanswong <jonathan.wong@tealium.com>
2019-07-23 10:55:02 -0700

    Refactored project and files.

---
+++ TealiumBraze/Sources/BrazeCommand.swift
@@ -0,0 +1,360 @@
+//
+//  BrazeCommand.swift
+//  RemoteCommandModules
+//
+//  Created by Jonathan Wong on 10/29/18.
+//  Copyright © 2018 Tealium. All rights reserved.
+//
+
+import UIKit
+import Appboy_iOS_SDK
+import TealiumSwift
+
+enum AppboyUserAttribute: String, CaseIterable {
+    case firstName = "first_name"
+    case lastName = "last_name"
+    case email
+    case dateOfBirth = "date_of_birth"
+    case country
+    case language
+    case homeCity = "home_city"
+    case phone
+    case avatarImageURL = "avatar_image_url"
+    case gender
+}
+
+enum AppboyUserGenderType: Int {
+    case male
+    case female
+    case other
+    case unknown
+    case notApplicable
+    case preferNotToSay
+    
+    static func from(_ value: String) -> AppboyUserGenderType {
+        let lowercasedGender = value.lowercased()
+        if lowercasedGender == "male" {
+            return .male
+        } else if lowercasedGender == "female" {
+            return .female
+        } else if lowercasedGender == "other" {
+            return .other
+        } else if lowercasedGender == "unknown" {
+            return .unknown
+        } else if lowercasedGender == "notapplicable" || lowercasedGender == "not_applicable" {
+            return .notApplicable
+        } else {
+            return .preferNotToSay
+        }
+    }
+}
+
+enum SocialMediaKey: String {
+    case userInfo = "user_info"
+    case friendsCount = "friends_count"
+    case likes = "likes"
+    case userDescription = "description"
+    case twitterName = "twitter_name"
+    case profileImageUrl = "profile_image_url"
+    case screenName = "screen_name"
+    case followersCount = "followers_count"
+    case statusesCount = "statuses_count"
+    case twitterId = "twitter_id"
+}
+
+enum AppboyNotificationSubscription: String {
+    case optedIn
+    case subscribed
+    case unsubscribed
+    
+    static func from(_ value: String) -> AppboyNotificationSubscription? {
+        let lowercasedSubscription = value.lowercased()
+        if lowercasedSubscription == "optedin" {
+            return .optedIn
+        } else if lowercasedSubscription == "subscribed" {
+            return .subscribed
+        } else if lowercasedSubscription == "unsubscribed" {
+            return .unsubscribed
+        } else {
+            return nil
+        }
+    }
+}
+
+class BrazeCommand {
+    
+    enum AppboyKey {
+        static let launchOptions = "launch_options"
+        static let pushToken = "push_token"
+        static let userAttribute = "user_attributes"
+        static let userIdentifier = "user_id"
+        static let userAlias = "user_alias"
+        static let aliasLabel = "alias_label"
+        static let customAttribute = "set_custom_attribute"
+        static let customArrayAttribute = "set_custom_array_attribute"
+        static let appendCustomArrayAttribute = "append_custom_array_attribute"
+        static let removeCustomArrayAttribute = "remove_custom_array_attribute"
+        static let unsetCustomAttribute = "unset_custom_attribute"
+        static let incrementCustomAttribute = "increment_custom_attribute"
+        static let emailNotification = "email_notification"
+        static let pushNotification = "push_notification"
+        static let eventProperties = "event_properties"
+        static let eventName = "event_name"
+        static let productIdentifier = "product_id"
+        static let currency = "currency"
+        static let price = "price"
+        static let quantity = "quantity"
+        static let purchaseProperties = "purchase_properties"
+        static let facebookUser = "facebook_id"
+        static let twitterUser = "twitter_user"
+        static let enableSDK = "enable_sdk"
+        static let sessionTimeout = "session_timeout"
+        static let disableLocation = "disable_location"
+        static let triggerIntervalSeconds = "trigger_interval_seconds"
+    }
+    
+    enum AppboyCommand {
+        static let initialize = "initialize"
+        static let userIdentifier = "useridentifier"
+        static let userAlias = "useralias"
+        static let userAttribute = "userattribute"
+        static let facebookUser = "facebookuser"
+        static let twitterUser = "twitteruser"
+        static let setCustomAttribute = "setcustomattribute"
+        static let unsetCustomAttribute = "unsetcustomattribute"
+        static let setCustomArrayAttribute = "setcustomarrayattribute"
+        static let appendCustomArrayAttribute = "appendcustomarrayattribute"
+        static let removeCustomArrayAttribute = "removecustomarrayattribute"
+        static let emailNotification = "emailnotification"
+        static let pushNotification = "pushnotification"
+        static let incrementCustomAttribute = "incrementcustomattribute"
+        static let logCustomEvent = "logcustomevent"
+        static let logPurchase = "logpurchase"
+        static let enableSDK = "enablesdk"
+        static let wipeData = "wipedata"
+    }
+    
+    enum AppboyOption {
+        static let ABKDisableAutomaticLocationCollectionKey = "ABKDisableAutomaticLocationCollectionKey"
+        static let ABKSessionTimeoutKey = "ABKSessionTimeoutKey"
+        static let ABKMinimumTriggerTimeIntervalKey = "ABKMinimumTriggerTimeIntervalKey"
+    }
+    
+    let brazeCommandRunner: BrazeCommandRunnable
+    
+    init(brazeCommandRunner: BrazeCommandRunnable) {
+        self.brazeCommandRunner = brazeCommandRunner
+    }
+    
+    /// Parses the remote command
+    func remoteCommand() -> TealiumRemoteCommand {
+        return TealiumRemoteCommand(commandId: "braze", description: "Braze Remote Command") { response in
+            let payload = response.payload()
+            guard let command = payload[TealiumRemoteCommand.commandName] as? String else {
+                return
+            }
+            
+            let commands = command.split(separator: ",")
+            let brazeCommands = commands.map { command in
+                return command.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            }
+            
+            brazeCommands.forEach { command in
+                let lowercasedCommand = command.lowercased()
+                switch lowercasedCommand {
+                case AppboyCommand.initialize:
+                    var appboyOptions = [String: Any]()
+                    guard let apiKey = payload[TealiumRemoteCommand.apiKey] as? String else {
+                        return
+                    }
+                    if let sessionTimeout = payload[AppboyKey.sessionTimeout] as? Int {
+                        appboyOptions += [AppboyOption.ABKSessionTimeoutKey: sessionTimeout]
+                    }
+                    if let disableLocation = payload[AppboyKey.disableLocation] as? Bool {
+                        appboyOptions += [AppboyOption.ABKDisableAutomaticLocationCollectionKey: disableLocation]
+                    }
+                    if let triggerInterval = payload[AppboyKey.triggerIntervalSeconds] as? Int {
+                        appboyOptions += [AppboyOption.ABKMinimumTriggerTimeIntervalKey: triggerInterval]
+                    }
+                    guard let launchOptions = payload[AppboyKey.launchOptions] as? [UIApplication.LaunchOptionsKey: Any] else {
+                        return self.brazeCommandRunner.initializeBraze(apiKey: apiKey, application: UIApplication.shared, launchOptions: nil, appboyOptions: appboyOptions)
+                    }
+                    self.brazeCommandRunner.initializeBraze(apiKey: apiKey, application: UIApplication.shared, launchOptions: launchOptions, appboyOptions: appboyOptions)
+                case AppboyCommand.userIdentifier:
+                    guard let userIdentifier = payload[AppboyKey.userIdentifier] as? String else {
+                        return
+                    }
+                    self.brazeCommandRunner.changeUser(userIdentifier)
+                case AppboyCommand.userAlias:
+                    guard let userAlias = payload[AppboyKey.userAlias] as? String, let label = payload[AppboyKey.aliasLabel] as? String else {
+                        return
+                    }
+                    self.brazeCommandRunner.addAlias(userAlias, label: label)
+                case AppboyCommand.userAttribute:
+                    self.brazeCommandRunner.setUserAttributes(payload)
+                case AppboyCommand.facebookUser:
+                    guard var facebookInfo = payload[AppboyKey.facebookUser] as? [String: Any] else {
+                        return
+                    }
+                    if let numberOfFriends = payload[SocialMediaKey.friendsCount.rawValue] as? Int {
+                        facebookInfo[SocialMediaKey.friendsCount.rawValue] = numberOfFriends
+                    }
+                    if let facebookLikes = payload[SocialMediaKey.likes.rawValue] as? NSArray {
+                        facebookInfo[SocialMediaKey.likes.rawValue] = facebookLikes
+                    }
+                    self.brazeCommandRunner.setFacebookUser(facebookInfo)
+                case AppboyCommand.twitterUser:
+                    var twitterInfo = [String: Any]()
+                    if let userDescription = payload[SocialMediaKey.userDescription.rawValue] as? String {
+                        twitterInfo[SocialMediaKey.userDescription.rawValue] = userDescription
+                    }
+                    if let twitterName = payload[SocialMediaKey.twitterName.rawValue] as? String {
+                        twitterInfo[SocialMediaKey.twitterName.rawValue] = twitterName
+                    }
+                    if let profileImageUrl = payload[SocialMediaKey.profileImageUrl.rawValue] as? String {
+                        twitterInfo[SocialMediaKey.profileImageUrl.rawValue] = profileImageUrl
+                    }
+                    if let screenName = payload[SocialMediaKey.screenName.rawValue] as? String {
+                        twitterInfo[SocialMediaKey.screenName.rawValue] = screenName
+                    }
+                    if let followersCount = payload[SocialMediaKey.followersCount.rawValue] as? Int {
+                        twitterInfo[SocialMediaKey.followersCount.rawValue] = followersCount
+                    }
+                    if let friendsCount = payload[SocialMediaKey.friendsCount.rawValue] as? Int {
+                        twitterInfo[SocialMediaKey.friendsCount.rawValue] = friendsCount
+                    }
+                    if let statusesCount = payload[SocialMediaKey.statusesCount.rawValue] as? Int {
+                        twitterInfo[SocialMediaKey.statusesCount.rawValue] = statusesCount
+                    }
+                    if let twitterId = payload[SocialMediaKey.twitterId.rawValue] as? Int {
+                        twitterInfo[SocialMediaKey.twitterId.rawValue] = twitterId
+                    }
+                    self.brazeCommandRunner.setTwitterUser(twitterInfo)
+                case AppboyCommand.logCustomEvent:
+                    guard let eventName = payload[AppboyKey.eventName] as? String else {
+                        return
+                    }
+                    guard let properties = payload[AppboyKey.eventProperties] as? [String: Any] else {
+                        return self.brazeCommandRunner.logCustomEvent(eventName: eventName)
+                    }
+                    self.brazeCommandRunner.logCustomEvent(eventName, properties: properties)
+                case AppboyCommand.setCustomAttribute:
+                    guard let attributes = payload[AppboyKey.customAttribute] as? [String: Any] else {
+                        return
+                    }
+                    self.brazeCommandRunner.setCustomAttributes(attributes)
+                case AppboyCommand.unsetCustomAttribute:
+                    guard let attributeKey = payload[AppboyKey.unsetCustomAttribute] as? String else {
+                        return
+                    }
+                    self.brazeCommandRunner.unsetCustomAttributeWithKey(attributeKey)
+                case AppboyCommand.incrementCustomAttribute:
+                    guard let attributes = payload[AppboyKey.incrementCustomAttribute] as? [String: Int] else {
+                        return
+                    }
+                    self.brazeCommandRunner.incrementCustomUserAttributes(attributes)
+                case AppboyCommand.setCustomArrayAttribute:
+                    guard let customAttributes = payload[AppboyKey.customArrayAttribute] as? [String: [Any]] else {
+                        return
+                    }
+                    _ = customAttributes.compactMap { key, value in
+                        self.brazeCommandRunner.setCustomAttributeArrayWithKey(key, array: value)
+                    }
+                case AppboyCommand.appendCustomArrayAttribute:
+                    guard let customAttributes = payload[AppboyKey.appendCustomArrayAttribute] as? [String: String] else {
+                        return
+                    }
+                    _ = customAttributes.map { key, value in
+                        self.brazeCommandRunner.addToCustomAttributeArrayWithKey(key, value: value)
+                    }
+                case AppboyCommand.removeCustomArrayAttribute:
+                    guard let customAttributes = payload[AppboyKey.removeCustomArrayAttribute] as? [String: String] else {
+                        return
+                    }
+                    _ = customAttributes.map { key, value in
+                        self.brazeCommandRunner.removeFromCustomAttributeArrayWithKey(key, value: value)
+                    }
+                case AppboyCommand.emailNotification:
+                    guard let emailNotification = payload[AppboyKey.emailNotification] as? String, let subscriptionType = AppboyNotificationSubscription.from(emailNotification) else {
+                        return
+                    }
+                    self.brazeCommandRunner.setEmailNotificationSubscriptionType(value: subscriptionType)
+                case AppboyCommand.pushNotification:
+                    guard let pushNotification = payload[AppboyKey.pushNotification] as? String, let subscriptionType = AppboyNotificationSubscription.from(pushNotification) else {
+                        return
+                    }
+                    self.brazeCommandRunner.setPushNotificationSubscriptionType(value: subscriptionType)
+                case AppboyCommand.logPurchase:
+                    guard let productIdentifier = payload[AppboyKey.productIdentifier] as? [String],
+                        let currency = payload[AppboyKey.currency] as? String,
+                        let priceDouble = payload[AppboyKey.price] as? [Double] else {
+                            return
+                    }
+                    let prices = priceDouble.map { price in
+                        NSDecimalNumber(floatLiteral: price)
+                        
+                    }
+                    var products = (productId: productIdentifier, price: prices)
+                    
+                    if let quantity = payload[AppboyKey.quantity] as? [Int] {
+                        let unsignedQty = quantity.map { UInt($0) }
+                        var products = (productId: productIdentifier, price: prices, quantity: unsignedQty)
+                        if let properties = payload[AppboyKey.purchaseProperties] as? [AnyHashable: Any] {
+                            for (index, element) in products.productId.enumerated() {
+                                return self.brazeCommandRunner.logPurchase(element, currency: currency, price: products.price[index], quantity: products.quantity[index], properties: properties)
+                            }
+                        }
+                        for (index, element) in products.productId.enumerated() {
+                            return self.brazeCommandRunner.logPurchase(element, currency: currency, price: products.price[index], quantity: products.quantity[index])
+                        }
+                    } else if let properties = payload[AppboyKey.purchaseProperties] as? [AnyHashable: Any] {
+                        for (index, element) in products.productId.enumerated() {
+                            self.brazeCommandRunner.logPurchase(element, currency: currency, price: products.price[index], properties: properties)
+                        }
+                    } else {
+                        for (index, element) in products.productId.enumerated() {
+                            self.brazeCommandRunner.logPurchase(element, currency: currency, price: products.price[index])
+                        }
+                    }
+                case AppboyCommand.enableSDK:
+                    guard let enabled = payload[AppboyKey.enableSDK] as? Bool else {
+                        return
+                    }
+                    self.brazeCommandRunner.enableSDK(enabled)
+                case AppboyCommand.wipeData:
+                    self.brazeCommandRunner.wipeData()
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    func setUserAttribute(value: AppboyUserGenderType) {
+        // currently only one int type
+        var gender: ABKUserGenderType
+        switch value {
+        case .male:
+            gender = ABKUserGenderType.male
+        case .female:
+            gender = ABKUserGenderType.female
+        case .other:
+            gender = ABKUserGenderType.other
+        case .unknown:
+            gender = ABKUserGenderType.unknown
+        case .notApplicable:
+            gender = ABKUserGenderType.notApplicable
+        case .preferNotToSay:
+            gender = ABKUserGenderType.preferNotToSay
+        }
+        
+        Appboy.sharedInstance()?.user.setGender(gender)
+    }
+    
+}
+
+extension TealiumRemoteCommand {
+    static let commandName = "command_name"
+    static let apiKey = "api_key"
+}
+
---
+++ TealiumBraze/TealiumBrazeExample/TealiumBrazeExample/EventViewController.swift
@@ -0,0 +1,67 @@
+//
+//  EventViewController.swift
+//  BrazeRemoteCommand
+//
+//  Created by Jonathan Wong on 5/30/19.
+//  Copyright © 2019 Jonathan Wong. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    static var arrayCounter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func logEvent(_ sender: UIButton) {
+        TealiumHelper.track(title: "log_custom_event", data: ["event_name": "custom_event"])
+    }
+    
+    @IBAction func logEventWithProperties(_ sender: Any) {
+        let eventProperties: [String: Any] = [
+            "key1": "value1",
+            "key2": "value2"]
+        TealiumHelper.track(title: "log_custom_event", data: ["event_name": "custom_event_with_properties",
+                                                              "event_properties": eventProperties
+            ])
+    }
+    
+    @IBAction func setCustomAttributes(_ sender: UIButton) {
+        let customAttributes: [String: Any] = ["pet": "cat",
+                                "pet_count": 3]
+        TealiumHelper.track(title: "custom_attribute", data: customAttributes)
+        
+        let customArrayAttributes: [String: Any] = ["pet_names": ["Rosia", "Elsa", "Kawai"]]
+        TealiumHelper.track(title: "custom_array_attribute", data: customArrayAttributes)
+    }
+    
+    @IBAction func unsetCustomAttributes(_ sender: UIButton) {
+        let customAttributes = ["pet_count_unset": "pet_count"]
+        TealiumHelper.track(title: "unset_custom_attribute", data: customAttributes)
+        
+        let removeCustomArrayAttributes = ["pet_names_remove": "Kawai"]
+        TealiumHelper.track(title: "remove_custom_array_attribute", data: removeCustomArrayAttributes)
+    }
+    
+    
+    @IBAction func incrementCustomAttributes(_ sender: Any) {
+        let customAttributes = ["pet_count_increment": 2]
+        TealiumHelper.track(title: "increment_custom_attribute", data: customAttributes)
+        
+        let appendCustomArrayAttributes: [String: Any] = ["pet_names_append": "petname\(EventViewController.arrayCounter)"]
+        EventViewController.arrayCounter += 1
+        TealiumHelper.track(title: "append_custom_array_attribute", data: appendCustomArrayAttributes)
+    }
+    
+    
+    @IBAction func logPurchase(_ sender: Any) {
+        var purchaseInfo = [String: Any]()
+        purchaseInfo["product_id"] = ["sku123"]
+        purchaseInfo["order_currency"] = "usd"
+        purchaseInfo["price"] = 1.99
+        TealiumHelper.track(title: "log_purchase", data: purchaseInfo)
+    }
+}
