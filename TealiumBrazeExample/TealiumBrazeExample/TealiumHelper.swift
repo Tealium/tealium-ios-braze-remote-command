22bf3313daf43ebc2d535798bd02a11b9654844a
Enrico Zannini <enricozannini93@gmail.com>
2022-09-19 17:55:44 +0200

    Test new all_events command mapping

--- TealiumBrazeExample/TealiumBrazeExample/TealiumHelper.swift
+++ TealiumBrazeExample/TealiumBrazeExample/TealiumHelper.swift
@@ -29,7 +29,8 @@ class TealiumHelper {
     static var universalData = [String: Any]()
     
     // JSON Remote Command
-    let brazeRemoteCommand = BrazeRemoteCommand(type: .remote(url: "https://tags.tiqcdn.com/dle/tealiummobile/demo/braze.json"))
+    let brazeRemoteCommand = BrazeRemoteCommand(type: .local(file:"braze"))
+//    let brazeRemoteCommand = BrazeRemoteCommand(type: .remote(url: "https://tags.tiqcdn.com/dle/tealiummobile/demo/braze.json"))
     
     private init() {
         config.shouldUseRemotePublishSettings = false
