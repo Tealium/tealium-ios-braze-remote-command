81eca47d1e9c483b5a0815f1243e390557f532b3
Craig Rouse <craig.rouse@tealium.com>
2024-04-22 16:10:31 +0100

    MT-1524 - Upgrade Braze SDK version
    * Bumped Braze dependency versions for all package managers

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.12.0")),
-        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "7.2.0"))
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "8.0.0"))
     ],
     targets: [
         .target(
