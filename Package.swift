ca74fd6ba5f04f146942b068187999b5477201dd
Enrico Zannini <enricozannini93@gmail.com>
2025-04-21 19:22:41 +0200

    Update Cartfile and Package.swift (#27)
    update xcframeworks script to be more precise when selecting the code signing certificate

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "TealiumBraze", targets: ["TealiumBraze"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.12.0")),
-        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "9.0.0"))
+        .package(url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.16.0")),
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "11.0.0"))
     ],
     targets: [
         .target(
