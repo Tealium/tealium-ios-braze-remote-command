a346ff54ce4376c174d556f60efdb61828aa021e
Sebastian Krajna <sebastian.krajna@tealium.com>
2026-09-14 17:48:02 +0200

    Refactor BrazeCommand protocol to improve readability by removing unnecessary whitespace and updating logPurchase method parameters for default values.

--- Sources/BrazeInstance.swift
+++ Sources/BrazeInstance.swift
@@ -18,60 +18,60 @@ import TealiumRemoteCommands
 #endif
 
 public protocol BrazeCommand {
-    
+
     var braze: Braze? { get }
-    
+
     func onReady(_ onReady: @escaping (Braze) -> Void)
 
     func flush()
-    
+
     // MARK: Initialization
     func initializeBraze(brazeConfig: Braze.Configuration)
-    
+
     // MARK: User IDs
     func changeUser(_ userIdentifier: String, sdkAuthSignature: String?)
-    
+
     func setSdkAuthenticationSignature(_ signature: String)
-    
+
     func addAlias(_ aliasName: String, label: String)
-    
+
     // MARK: Events
     func logCustomEvent(eventName: String)
-    
+
     func logCustomEvent(_ eventName: String, properties: [String: Any])
-    
+
     func setAdTrackingEnabled(_ enabled: Bool)
-    
+
     func setIdentifierForAdvertiser(_ identifier: String)
-    
+
     func setIdentifierForVendor(_ identifier: String)
-    
+
     // MARK: Attributes
-    
+
     func setUserAttributes(_ attributes: [String: Any])
-    
+
     func setCustomAttributes(_ attributes: [String: Any])
-    
+
     func unsetCustomAttributeWithKey(_ key: String)
-    
+
     func incrementCustomUserAttributes(_ attributes: [String: Int])
-    
+
     // MARK: Array Attributes
     func setCustomAttributeArrayWithKey(_ key: String, array: [String]?)
-    
+
     func addToCustomAttributeArrayWithKey(_ key: String, value: String)
-    
+
     func removeFromCustomAttributeArrayWithKey(_ key: String, value: String)
-    
+
     // MARK: Subscriptions
     func setEmailNotificationSubscriptionType(value: Braze.User.SubscriptionState)
-    
+
     func setPushNotificationSubscriptionType(value: Braze.User.SubscriptionState)
-    
+
     func addToSubscriptionGroup(_ group: String)
-    
+
     func removeFromSubscriptionGroup(_ group: String)
-    
+
     // MARK: Purchases
     func logPurchase(_ productIdentifier: String,
                      currency: String,
@@ -90,10 +90,10 @@ public protocol BrazeCommand {
                                           horizontalAccuracy: Double,
                                           altitude: Double,
                                           verticalAccuracy: Double)
-    
+
     // MARK: Enabling/Wiping
     func enableSDK(_ enable: Bool)
-    
+
     func wipeData()
 }
 
@@ -101,68 +101,68 @@ public class BrazeInstance: BrazeCommand {
     public var braze: Braze? {
         _onReady.last()
     }
-    
+
     private var _onReady = TealiumReplaySubject<Braze>(cacheSize: 1)
 
     public init() { }
-    
+
     public func initializeBraze(brazeConfig: Braze.Configuration) {
         let braze = Braze(configuration: brazeConfig)
         _onReady.publish(braze)
     }
-    
+
     public func onReady(_ onReady: @escaping (Braze) -> Void) {
         _onReady.subscribeOnce(onReady)
     }
-    
+
     public func changeUser(_ userIdentifier: String, sdkAuthSignature: String?) {
         onReady { braze in
             braze.changeUser(userId: userIdentifier, sdkAuthSignature: sdkAuthSignature)
         }
     }
-    
+
     public func setSdkAuthenticationSignature(_ signature: String) {
         onReady { braze in
             braze.set(sdkAuthenticationSignature: signature)
         }
     }
-    
+
     public func addAlias(_ aliasName: String, label: String) {
         onReady { braze in
             braze.user.add(alias: aliasName, label: label)
         }
     }
-    
+
     public func logCustomEvent(eventName: String) {
         onReady { braze in
             braze.logCustomEvent(name: eventName)
         }
     }
-    
+
     public func logCustomEvent(_ eventName: String, properties: [String: Any]) {
         onReady { braze in
             braze.logCustomEvent(name: eventName, properties: properties)
         }
     }
-    
+
     public func setAdTrackingEnabled(_ enabled: Bool) {
         onReady { braze in
             braze.set(adTrackingEnabled: enabled)
         }
     }
-    
+
     public func setIdentifierForAdvertiser(_ identifier: String) {
         onReady { braze in
             braze.set(identifierForAdvertiser: identifier)
         }
     }
-    
+
     public func setIdentifierForVendor(_ identifier: String) {
         onReady { braze in
             braze.set(identifierForVendor: identifier)
         }
     }
-    
+
     public func setUserAttributes(_ attributes: [String : Any]) {
         onReady { braze in
             AppboyUserAttribute.allCases.forEach {
@@ -173,7 +173,7 @@ public class BrazeInstance: BrazeCommand {
             }
         }
     }
-    
+
     static private func setUserAttribute(_ braze: Braze, key: AppboyUserAttribute, value: String) {
         switch key {
         case .firstName:
@@ -199,7 +199,7 @@ public class BrazeInstance: BrazeCommand {
             braze.user.set(gender: Braze.User.Gender.from(value))
         }
     }
-    
+
     public func setCustomAttributes(_ attributes: [String : Any]) {
         onReady {  braze in
             attributes.forEach { attribute in
@@ -210,7 +210,7 @@ public class BrazeInstance: BrazeCommand {
             }
         }
     }
-    
+
     static private func setCustomAttribute(_ braze: Braze, withKey key: String, value: AnyHashable) {
         if let value = value as? Bool {
             braze.user.setCustomAttribute(key: key, value: value)
@@ -230,37 +230,37 @@ public class BrazeInstance: BrazeCommand {
             braze.user.setCustomAttribute(key: key, array: value)
         }
     }
-    
+
     public func unsetCustomAttributeWithKey(_ key: String) {
         onReady { braze in
             braze.user.unsetCustomAttribute(key: key)
         }
     }
-    
+
     public func setCustomAttributeArrayWithKey(_ key: String, array: [String]?) {
         onReady { braze in
             braze.user.setCustomAttribute(key: key, array: array)
         }
     }
-    
+
     public func addToCustomAttributeArrayWithKey(_ key: String, value: String) {
         onReady { braze in
             braze.user.addToCustomAttributeStringArray(key: key, value: value)
         }
     }
-    
+
     public func removeFromCustomAttributeArrayWithKey(_ key: String, value: String) {
         onReady { braze in
             braze.user.removeFromCustomAttributeStringArray(key: key, value: value)
         }
     }
-    
+
     public func setEmailNotificationSubscriptionType(value: Braze.User.SubscriptionState) {
         onReady { braze in
             braze.user.set(emailSubscriptionState: value)
         }
     }
-    
+
     public func setPushNotificationSubscriptionType(value: Braze.User.SubscriptionState) {
         onReady { braze in
             braze.user.set(pushNotificationSubscriptionState: value)
@@ -274,12 +274,12 @@ public class BrazeInstance: BrazeCommand {
             }
         }
     }
-    
+
     public func logPurchase(_ productIdentifier: String,
                             currency: String,
                             price: Double,
-                            quantity: Int,
-                            properties: [String: Any]?) {
+                            quantity: Int = 1,
+                            properties: [String: Any]? = nil) {
         onReady { braze in
             braze.logPurchase(productId: productIdentifier,
                               currency: currency,
@@ -288,7 +288,7 @@ public class BrazeInstance: BrazeCommand {
                               properties: properties)
         }
     }
-    
+
     public func logEcommerceEvent<E: Braze.Ecommerce.Event>(_ event: E) {
         onReady { braze in
             braze.logEcommerceEvent(event)
@@ -318,31 +318,31 @@ public class BrazeInstance: BrazeCommand {
                                             verticalAccuracy: verticalAccuracy)
         }
     }
-    
+
     public func flush() {
         onReady { braze in
             braze.requestImmediateDataFlush()
         }
     }
-    
+
     public func addToSubscriptionGroup(_ group: String) {
         onReady { braze in
             braze.user.addToSubscriptionGroup(id: group)
         }
     }
-    
+
     public func removeFromSubscriptionGroup(_ group: String) {
         onReady { braze in
             braze.user.removeFromSubscriptionGroup(id: group)
         }
     }
-    
+
     public func enableSDK(_ enable: Bool) {
         onReady { braze in
             braze.enabled = enable
         }
     }
-    
+
     public func wipeData() {
         onReady { braze in
             braze.wipeData()
